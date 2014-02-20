c2b752dc0fe894424717c0bb9f3990b1995f8545
Guest <guest-F7fqNh@meet.mit.edu>
2014-02-20 20:01:58 +0200

    lab4

---
+++ lab4/number1.py
@@ -0,0 +1,14 @@
+class number(object)
+	def __init__(self,number,positive)
+		self.number=number
+		self.positive=positive
+	def display(self):
+		if(self.positive==True):
+        		print self.number
+		else print 
+if__name__=="_main_":
+	test=number(9,'+')
+	test.display()
+
+
+		
